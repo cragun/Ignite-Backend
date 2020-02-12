fc3f43915fafc70aff71516d9db26917c2a42a34
mdhakech <mdhakecha@gmail.com>
2020-02-12 10:59:48 +0530

    Add New finance plan

--- DataReef.TM.Api/Controllers/FinancePlanDefinitionsController.cs
+++ DataReef.TM.Api/Controllers/FinancePlanDefinitionsController.cs
@@ -48,15 +48,15 @@ namespace DataReef.TM.Api.Controllers
         //    throw new HttpResponseException(HttpStatusCode.Forbidden);
         //}
 
-        public override ICollection<FinancePlanDefinition> PostMany(List<FinancePlanDefinition> items)
-        {
-            throw new HttpResponseException(HttpStatusCode.Forbidden);
-        }
+        //public override ICollection<FinancePlanDefinition> PostMany(List<FinancePlanDefinition> items)
+        //{
+        //    throw new HttpResponseException(HttpStatusCode.Forbidden);
+        //}
 
-        public override FinancePlanDefinition Put(FinancePlanDefinition item)
-        {
-            throw new HttpResponseException(HttpStatusCode.Forbidden);
-        }
+        //public override FinancePlanDefinition Put(FinancePlanDefinition item)
+        //{
+        //    throw new HttpResponseException(HttpStatusCode.Forbidden);
+        //}
 
         public override HttpResponseMessage Delete(FinancePlanDefinition item)
         {
