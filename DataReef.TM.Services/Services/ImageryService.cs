3c2fb75615ed7860d84c00b6e7d345da2063563e
mdhakech <mdhakecha@gmail.com>
2020-05-15 14:01:42 +0530

    HiRes image testing

--- DataReef.TM.Services/Services/ImageryService.cs
+++ DataReef.TM.Services/Services/ImageryService.cs
@@ -107,7 +107,6 @@ namespace DataReef.TM.Services.Services
 #if (DEBUG)
                 userID = new Guid("7D49D3AF-C08C-4AA9-AC76-A6652A8EE884"); //make sure there is an account.  this is Vlads test account
 #endif
-
                 if (!freeHiResImages)
                 {
                     ledger = _tokensProvider.GetDefaultLedgerForPerson(userID);
