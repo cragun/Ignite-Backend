b2c1f34da4b550ef29b818519919f5a1f64d7b0f
Hevin Techno Web <ankita@hevintechnoweb.com>
2020-06-24 12:47:03 +0530

    Proposal api changes

--- DataReef.TM.Services/Services/ProposalService.cs
+++ DataReef.TM.Services/Services/ProposalService.cs
@@ -2300,6 +2300,13 @@ namespace DataReef.TM.Services.Services
         {
             using (var dataContext = new DataContext())
             {
+                var existingadderItem = dataContext.AdderItems.FirstOrDefault(i => i.Type == adderItem.Type && i.Name == adderItem.Name && i.Cost == adderItem.Cost && i.Guid == ProposalID);
+
+                if (existingadderItem != null)
+                {
+                    throw new Exception("Already added");
+                }
+
                 adderItem = AdderItem.ToDbModel(adderItem, ProposalID);
                 dataContext.AdderItems.Add(adderItem);
                 dataContext.SaveChanges();
