1cf672702965dc9b9ed4c7021c20e16666e1b6aa
mdhakech <mdhakecha@gmail.com>
2020-07-28 15:00:36 +0530

    Revert "#393 api changes"
    This reverts commit 9bf67acc0507fb8f3237f84061e6a931787ebdde.

--- DataReef.TM.Models/DTOs/AddUserStartDateRequest.cs
+++ DataReef.TM.Models/DTOs/AddUserStartDateRequest.cs
@@ -9,6 +9,7 @@ namespace DataReef.TM.Models.DTOs
     public class AddUserStartDateRequest
     {
         public Guid UserId { get; set; }
+        public string ApiKey { get; set; }
         public DateTime Date { get; set; }
 
     }
