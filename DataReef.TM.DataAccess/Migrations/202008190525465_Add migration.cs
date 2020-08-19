3d498fdcb1b545aff638b697964c71092c100497
mdhakech <mdhakecha@gmail.com>
2020-08-19 14:28:54 +0530

    territory assign

---
+++ DataReef.TM.DataAccess/Migrations/202008190525465_Add migration.cs
@@ -0,0 +1,18 @@
+namespace DataReef.TM.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Addmigration : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
