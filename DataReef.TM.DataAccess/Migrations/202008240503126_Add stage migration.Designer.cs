8f051323a80983cb5027c764f482b714bf2a696d
mdhakech <mdhakecha@gmail.com>
2020-08-24 10:39:39 +0530

    migration changes

--- DataReef.TM.DataAccess/Migrations/202008240503126_Add stage migration.Designer.cs
+++ DataReef.TM.DataAccess/Migrations/202008240503126_Add stage migration.Designer.cs
@@ -7,13 +7,13 @@ namespace DataReef.TM.DataAccess.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class AddPrefixinAdapterRequeststable : IMigrationMetadata
+    public sealed partial class Addstagemigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddPrefixinAdapterRequeststable));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Addstagemigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "202006270522492_Add Prefix in AdapterRequests table"; }
+            get { return "202008240503126_Add stage migration"; }
         }
         
         string IMigrationMetadata.Source
