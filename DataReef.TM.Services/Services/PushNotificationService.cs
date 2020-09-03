c8a3dd4ceef41efdec5444a627e7127b99abe05f
AnkitaHevin <ankita@hevintechnoweb.com>
2020-09-03 22:27:23 +0530

    push notification

--- DataReef.TM.Services/Services/PushNotificationService.cs
+++ DataReef.TM.Services/Services/PushNotificationService.cs
@@ -20,18 +20,18 @@ namespace DataReef.TM.Services.Services
         private static readonly string serverKey = ConfigurationManager.AppSettings["Firebase.ServerKey"]; 
         private static readonly string url = "https://fcm.googleapis.com/fcm/send"; 
 
-        public string PushNotification(string token, string message, string title)
+        public string PushNotification(string message, string token, string title)
         { 
             try
             {
                 var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
                 httpWebRequest.ContentType = "application/json";
-                httpWebRequest.Headers.Add("Authorization", "key=AAAAjcK0I_g:APA91bE9yx0Ximczoh423GN5fUhOSG5XOYnLxHDJtciBdGcapueC9LhCe0xyMMJwnfY79UrZ83rPXhbQLvW_JOcbbT6xNy_P7U96YKfQXB_U2Zr5Um58Dk0TglI_pvRghEoll5AqfN94");
+                httpWebRequest.Headers.Add("Authorization", "key=" + serverKey);
                 httpWebRequest.Method = "POST";
 
                 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
-                    string json = "{\"to\": \"" + "cCgFluq1nE9QmEhmwJcPQI:APA91bEiWuHQbmgATcf2KBfCM37O3OeLPZyV9WOZrDT8i0w7f0KG4f_funNkTH-jCyFnBUW3N5eseDw5grZwfBEi4LoNBNwlmRf_w40z_qAqIYLlXqQLYhEdODm9M7X93oMchCKIIt9C" + "\",\"notification\": {\"title\": \"" + title + "\",\"body\": \"" + message + "\"},\"priority\":10}";
+                    string json = "{\"to\": \"" + token + "\",\"notification\": {\"title\": \"" + title + "\",\"body\": \"" + message + "\"},\"priority\":10}";
                     streamWriter.Write(json);
                     streamWriter.Flush();
                 }
