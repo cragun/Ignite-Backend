0ebafac5988d39dbed6eb03019a001480a97d922
AnkitaHevin <ankita@hevintechnoweb.com>
2020-09-26 15:15:25 +0530

    Check Deploy

--- DataReef.TM.Services/Services/ProposalService.cs
+++ DataReef.TM.Services/Services/ProposalService.cs
@@ -2462,7 +2462,7 @@ namespace DataReef.TM.Services.Services
 
                 existingadderItem.Quantity = adderItem.Quantity == 0 ? 1 : adderItem.Quantity;
 
-                //update Finance Plan 
+                //update Finance Plan  
                 var existingFinancePlan = dataContext.FinancePlans.FirstOrDefault(i => i.Guid == existingProposal.FinancePlanID);
                 if (existingFinancePlan == null)
                 {
