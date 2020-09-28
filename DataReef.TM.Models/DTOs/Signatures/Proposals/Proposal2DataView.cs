96c7b4a24530033a00008339a58fc560d600ac0a
mdhakecha@gmail.com <hevin123>
2020-09-28 23:57:07 +0530

    production param added

--- DataReef.TM.Models/DTOs/Signatures/Proposals/Proposal2DataView.cs
+++ DataReef.TM.Models/DTOs/Signatures/Proposals/Proposal2DataView.cs
@@ -49,6 +49,16 @@ namespace DataReef.TM.Models.DTOs.Signatures.Proposals
         public string ExcludeProposalJSON { get; set; }
 
 
+       
+        public double ProductionKWH { get; set; }
+    
+        public double ProductionKWHpercentage { get; set; }
+ 
+        public bool IsManual { get; set; }
+        
+        public double SystemSize { get; set; }
+
+
         public Proposal2DataView()
         {
             BasicInfo = new ProposalBasicInfo();
@@ -64,6 +74,11 @@ namespace DataReef.TM.Models.DTOs.Signatures.Proposals
             ProposalID = param.Proposal.Guid;
             SignedDate = param.Data.SignatureDate;
             UsageCollected = param.Proposal.Property.UsageCollected;
+            ProductionKWH = param.Proposal.ProductionKWH;
+            ProductionKWHpercentage = param.Proposal.ProductionKWHpercentage;
+            IsManual = param.Proposal.IsManual;
+            SystemSize = param.Proposal.SystemSize;
+
             ExcludeProposalJSON = param.Data.excludeProposalJSON ?? "[]";
             if (!string.IsNullOrEmpty(param.Proposal?.SignedDocumentsJSON))
             {
