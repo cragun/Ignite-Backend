67b79cca7f85895c50325e7eb896f0a3c8cb9b7a
mdhakecha@gmail.com <hevin123>
2020-11-27 19:37:54 +0530

    deploy test

--- DataReef.TM.Services/Services/PersonService.cs
+++ DataReef.TM.Services/Services/PersonService.cs
@@ -1194,8 +1194,6 @@ namespace DataReef.TM.Services
             }
         }
 
-
-
         public async Task<IEnumerable<Person>> CalendarPageAppointMentsByOuid(Guid ouid, string CurrentDate, string type)
         {
             try
