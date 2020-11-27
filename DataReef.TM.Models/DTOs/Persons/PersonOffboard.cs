7acec90cac84af13e2e761c8018a7b5892b8a8cd
mdhakecha@gmail.com <hevin123>
2020-11-27 16:40:45 +0530

    User Roles api

--- DataReef.TM.Models/DTOs/Persons/PersonOffboard.cs
+++ DataReef.TM.Models/DTOs/Persons/PersonOffboard.cs
@@ -8,7 +8,9 @@ namespace DataReef.TM.Models.DTOs.Persons
     public class PersonOffboard
     {
         public Guid OUID { get; set; }
+        public Guid RoleID { get; set; }
         public string AssociateOuName { get; set; }
         public string RoleName { get; set; }
+        public string Apikey { get; set; }
     }
 }
--- DataReef.TM.Services/Services/PersonService.cs
+++ DataReef.TM.Services/Services/PersonService.cs
@@ -1184,7 +1184,8 @@ namespace DataReef.TM.Services
                 var OUAssociation = dc.OUAssociations?.Include(p => p.OU).Include(p => p.OURole).Where(x => x.PersonID == personid && x.IsDeleted == false).ToList();
                 var OUAss = OUAssociation?.Select(x => new PersonOffboard
                 {
-                    OUID = x.Guid,
+                    RoleID = x.Guid,
+                    OUID = x.OUID,
                     AssociateOuName = x.OU?.Name,
                     RoleName = x.OURole?.Name
                 });
