edcc53843e1aea5130352dd07907d9d0d9411f89
AnkitaHevin <ankita@hevintechnoweb.com>
2020-12-02 19:09:09 +0530

    565 changes

--- DataReef.TM.Services/Services/OUService.cs
+++ DataReef.TM.Services/Services/OUService.cs
@@ -1898,7 +1898,7 @@ namespace DataReef.TM.Services.Services
 
                 foreach (var item in ignite)
                 {
-                    var isExist = sbclients.FirstOrDefault(a => a.Name == "Disposition.Options");
+                    var isExist = sbclients.FirstOrDefault(a => a.Name == item.Name);
                     if (isExist == null)
                     {
                         OUSetting setting = new OUSetting();
