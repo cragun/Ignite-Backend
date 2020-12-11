cfb63bdf59aa221cea5bedb19caf2a1531af705e
mdhakecha@gmail.com <hevin123>
2020-12-11 14:07:57 +0530

    #559 valid apikey

--- DataReef.TM.Services/Services/PropertyService.cs
+++ DataReef.TM.Services/Services/PropertyService.cs
@@ -1426,11 +1426,13 @@ namespace DataReef.TM.Services.Services
             }
         }
 
-        public async Task<IEnumerable<Territories>> GetTerritoryListbyApikey(string apiKey)
+        public async Task<IEnumerable<Territories>> GetTerritoryListbyApikey(string apiKey, double Lat, double Long)
         {
             using (var dc = new DataContext())
             {
-                var TerritoriesList = dc.Database.SqlQuery<Territories>("exec usp_GetTerritoryListByapiKeyOnly @apiKey", new SqlParameter("@apiKey", apiKey)).ToList();
+                //var TerritoriesList = dc.Database.SqlQuery<Territories>("exec usp_GetTerritoryListByapiKeyOnly @apiKey", new SqlParameter("@apiKey", apiKey)).ToList();
+
+                var TerritoriesList = dc.Database.SqlQuery<Territories>("exec usp_GetTerritoryIdsNameByapiKey @latitude, @longitude, @apiKey", new SqlParameter("@latitude", Lat), new SqlParameter("@longitude", Long), new SqlParameter("@apiKey", apiKey)).ToList();
 
                 return TerritoriesList;
             }
