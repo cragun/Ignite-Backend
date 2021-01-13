fc03b5eb6206629f723212a8f3ef3779ebbe9504
mdhakecha@gmail.com <hevin123>
2021-01-13 18:23:28 +0530

    deploy test

--- DataReef.TM.Services/Services/PropertyService.cs
+++ DataReef.TM.Services/Services/PropertyService.cs
@@ -442,8 +442,6 @@ namespace DataReef.TM.Services.Services
                                 //_smsService.Value.SendSms("New Appointment is created!", entity.GetMainPhoneNumber());
                                 _smsService.Value.SendSms("You have a solar appointment with " + creator?.Name + " on " + fstAppoint.StartDate.Date.ToShortDateString() + " at " + fstAppoint.StartDate.ToShortTimeString() + " , https://calendar.google.com/calendar/u/0/r/" +
                                  fstAppoint.StartDate.Year + "/" + fstAppoint.StartDate.Month + "/" + fstAppoint.StartDate.Day, entity.GetMainPhoneNumber());
-
-
                             }
 
                             if (fstAppoint?.SendSmsToEC == true)
