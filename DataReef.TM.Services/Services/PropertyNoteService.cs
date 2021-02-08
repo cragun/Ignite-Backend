4f43e0876838f1603fecfc089b8199396825c31d
mdhakecha@gmail.com <hevin123>
2021-02-08 23:41:24 +0530

    deploy test

--- DataReef.TM.Services/Services/PropertyNoteService.cs
+++ DataReef.TM.Services/Services/PropertyNoteService.cs
@@ -690,6 +690,7 @@ namespace DataReef.TM.Services.Services
             }
         }
 
+
         public SBNoteDTO EditNoteFromSmartboard(SBNoteDTO noteRequest, string apiKey)
         {
             using (var dc = new DataContext())
