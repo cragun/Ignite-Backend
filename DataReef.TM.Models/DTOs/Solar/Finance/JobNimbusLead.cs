17e9943819a5a24222535ee23b5a4491d8899d39
mdhakecha@gmail.com <hevin123>
2021-03-19 16:57:47 +0530

    Jobnimbus api

--- DataReef.TM.Models/DTOs/Solar/Finance/JobNimbusLead.cs
+++ DataReef.TM.Models/DTOs/Solar/Finance/JobNimbusLead.cs
@@ -163,8 +163,8 @@ namespace DataReef.TM.Models.DTOs.Solar.Finance
         public bool is_active { get; set; }
         public bool is_archived { get; set; }
         public List<related> related { get; set; }
-       // public List<primary> primary { get; set; }
-        public string primary { get; set; }
+        // public List<primary> primary { get; set; }
+        public primary primary { get; set; }
         public bool is_private { get; set; }
         public int record_type { get; set; }
         public string record_type_name { get; set; }
