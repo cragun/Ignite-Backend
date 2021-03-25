727784ca99323c28c3fda20fbb405342e00720dd
AnkitaHevin <ankita@hevintechnoweb.com>
2021-03-25 22:28:05 +0530

    Check Deploy

--- DataReef.TM.Services/Extensions/DataViewExtensions.cs
+++ DataReef.TM.Services/Extensions/DataViewExtensions.cs
@@ -444,6 +444,7 @@ internal static class DataViewExtensions
 
         //as per new calulation
         var monthlySavings = (year1.ElectricityBillWithoutSolar / 12) - (introMonthlyPayment + newUtilityBill);
+
         var annualSavings = monthlySavings * 12;
         var totalCost = data.AmountFinanced == 0 ? data.SolarSystemCost : data.AmountFinanced;
 
