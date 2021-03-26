3938c16736861b0f464bba6c57fc80c1da53b1e3
AnkitaHevin <ankita@hevintechnoweb.com>
2021-03-26 17:00:44 +0530

    Check Deploy

--- DataReef.TM.Services/Extensions/DataViewExtensions.cs
+++ DataReef.TM.Services/Extensions/DataViewExtensions.cs
@@ -454,7 +454,6 @@ internal static class DataViewExtensions
 
         var annualSavings = monthlySavings * 12;
         var totalCost = data.AmountFinanced == 0 ? data.SolarSystemCost : data.AmountFinanced;  
-      
 
         return new ProposalFinancePlanOption
         {
