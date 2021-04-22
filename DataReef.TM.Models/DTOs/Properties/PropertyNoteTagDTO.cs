03cb24883dff5c8d3a8218afc46f24ba8b4e86fa
AnkitaHevin <ankita@hevintechnoweb.com>
2021-04-22 12:23:11 +0530

    note server api changes

--- DataReef.TM.Models/DTOs/Properties/PropertyNoteTagDTO.cs
+++ DataReef.TM.Models/DTOs/Properties/PropertyNoteTagDTO.cs
@@ -8,14 +8,11 @@ using System.Threading.Tasks;
 namespace DataReef.TM.Models.DTOs.Properties
 {
     public class PropertyNoteTagDTO
-    {
-        [JsonProperty(PropertyName = "name")]
+    { 
         public string Name { get; set; }
+         
+        public string Email { get; set; } 
 
-        [JsonProperty(PropertyName = "email")]
-        public string Email { get; set; }
-
-        [JsonProperty(PropertyName = "tag")]
         public string Tag { get; set; }
     }
 }
