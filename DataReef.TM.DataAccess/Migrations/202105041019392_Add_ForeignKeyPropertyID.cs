f045dbee2d22d94dea30382cd8065d8a33a2fa33
AnkitaHevin <ankita@hevintechnoweb.com>
2021-05-04 15:50:08 +0530

    migration changes

---
+++ DataReef.TM.DataAccess/Migrations/202105041019392_Add_ForeignKeyPropertyID.cs
@@ -0,0 +1,16 @@
+namespace DataReef.TM.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_ForeignKeyPropertyID : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
