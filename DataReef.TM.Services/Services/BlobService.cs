3c957c8bf06e94f649c6b980722f81ec497bec04
AnkitaHevin <ankita@hevintechnoweb.com>
2021-06-14 18:31:42 +0530

    s3 secure changes

--- DataReef.TM.Services/Services/BlobService.cs
+++ DataReef.TM.Services/Services/BlobService.cs
@@ -126,7 +126,8 @@ namespace DataReef.TM.Services
                     Key = name.ToLowerInvariant(),
                     InputStream = stream,
                     ContentType = blob.ContentType,
-                    CannedACL = GetAccessRights(access)
+                    //CannedACL = GetAccessRights(access),
+                    CannedACL = S3CannedACL.Private
                 };
 
                 var response = Client.PutObject(request);
@@ -236,7 +237,8 @@ namespace DataReef.TM.Services
                 DestinationBucket = destinationBucket ?? _s3BucketName,
                 SourceKey = sourceName.ToLowerInvariant(),
                 DestinationKey = destinationName.ToLowerInvariant(),
-                CannedACL = GetAccessRights(access)
+                //CannedACL = GetAccessRights(access)
+                CannedACL = S3CannedACL.Private
             };
 
 
