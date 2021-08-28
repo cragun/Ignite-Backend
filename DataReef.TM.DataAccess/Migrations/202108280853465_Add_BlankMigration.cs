7c354aaad5ab787f7edfb4dad7be70a6c53ccb29
AnkitaHevin <ankita@hevintechnoweb.com>
2021-08-28 14:45:36 +0530

    migration changes

---
+++ DataReef.TM.DataAccess/Migrations/202108280853465_Add_BlankMigration.cs
@@ -0,0 +1,18 @@
+namespace DataReef.TM.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_BlankMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OUs", "OURoleID", c => c.Guid(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OUs", "OURoleID");
+        }
+    }
+}
